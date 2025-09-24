7b9824944ce18222c6bf1aa277134acbb832745a
Steve Shreeve <steve.shreeve@gmail.com>
2025-09-24 01:20:06 -0600

    Compiled files

--- cs290/lib/coffeescript/coffeescript.js
+++ cs290/lib/coffeescript/coffeescript.js
@@ -4,7 +4,7 @@
   // on Node.js/V8, or to run CoffeeScript directly in the browser. This module
   // contains the main entry functions for tokenizing, parsing, and compiling
   // source CoffeeScript into JavaScript.
-  var ES6Backend, FILE_EXTENSIONS, Lexer, SourceMap, base64encode, checkShebangLine, compile, getSourceMap, helpers, lexer, packageJson, parser, registerCompiled, withPrettyErrors;
+  var ES5Backend, FILE_EXTENSIONS, Lexer, SourceMap, base64encode, checkShebangLine, compile, getSourceMap, helpers, lexer, packageJson, parser, registerCompiled, withPrettyErrors;
 
   ({Lexer} = require('./lexer'));
 
@@ -14,7 +14,7 @@
 
   SourceMap = require('./sourcemap');
 
-  ES6Backend = require('./es6');
+  ES5Backend = require('./es5');
 
   // Require `package.json`, which is two levels above this file, as this file is
   // evaluated from `lib/coffeescript`.
@@ -114,7 +114,7 @@
         }
       }
     }
-    parser.yy.backend = new ES6Backend(options, parser.yy); // Inject Solar backend
+    parser.yy.backend = new ES5Backend(options, parser.yy); // Inject Solar backend
     nodes = parser.parse(tokens);
     // If all that was requested was a POJO representation of the nodes, e.g.
     // the abstract syntax tree (AST), we can stop now and just return that
@@ -229,7 +229,7 @@
   exports.nodes = withPrettyErrors(function(source, options) {
     var tokens;
     tokens = typeof source === 'string' ? lexer.tokenize(source, options) : source;
-    parser.yy.backend = new ES6Backend(options, parser.yy); // Inject Solar backend
+    parser.yy.backend = new ES5Backend(options, parser.yy); // Inject Solar backend
     return parser.parse(tokens);
   });
 
