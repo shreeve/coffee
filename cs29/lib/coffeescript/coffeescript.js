2f293c37a7b75da448f2ca11e509769eb6fd1d5b
Steve Shreeve <steve.shreeve@gmail.com>
2025-09-27 17:23:04 -0600

    Compiled files

--- cs29/lib/coffeescript/coffeescript.js
+++ cs29/lib/coffeescript/coffeescript.js
@@ -4,7 +4,7 @@
   // on Node.js/V8, or to run CoffeeScript directly in the browser. This module
   // contains the main entry functions for tokenizing, parsing, and compiling
   // source CoffeeScript into JavaScript.
-  var FILE_EXTENSIONS, Lexer, SourceMap, base64encode, checkShebangLine, compile, getSourceMap, helpers, lexer, packageJson, parser, registerCompiled, withPrettyErrors;
+  var ES5Backend, FILE_EXTENSIONS, Lexer, SourceMap, base64encode, checkShebangLine, compile, getSourceMap, helpers, lexer, packageJson, parser, registerCompiled, withPrettyErrors;
 
   ({Lexer} = require('./lexer'));
 
@@ -13,6 +13,8 @@
   helpers = require('./helpers');
 
   SourceMap = require('./sourcemap');
+
+  ES5Backend = require('./es5');
 
   // Require `package.json`, which is two levels above this file, as this file is
   // evaluated from `lib/coffeescript`.
@@ -112,6 +114,7 @@
         }
       }
     }
+    parser.yy.backend = new ES5Backend(options, parser.yy); // Inject Solar backend
     nodes = parser.parse(tokens);
     // If all that was requested was a POJO representation of the nodes, e.g.
     // the abstract syntax tree (AST), we can stop now and just return that
@@ -227,6 +230,7 @@
     if (typeof source === 'string') {
       source = lexer.tokenize(source, options);
     }
+    parser.yy.backend = new ES5Backend(options, parser.yy); // Inject Solar backend
     return parser.parse(source);
   });
 
